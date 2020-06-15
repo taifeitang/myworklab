2545122ed3486395bc5e378893f17fc79ef2a9db
taifeitang <1026919284@qq.com>
2020-06-15 15:33:22 +0800

    热尔为

---
+++ src/main/java/com/wdx/MyworklabApplication.java
@@ -0,0 +1,13 @@
+package com.wdx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyworklabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyworklabApplication.class, args);
+    }
+
+}
